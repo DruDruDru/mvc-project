27a5de75d255b56f139cf2859ad7a1615578cb4d
DruDruDru <borisrasgard251@gmail.com>
2024-03-12 21:50:28 +0700

    добавление метода sighup в контроллер

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -5,6 +5,7 @@ namespace Controller;
 use Src\Request;
 use Src\View;
 use Model\Post;
+use Model\User;
 
 class Site
 {
@@ -14,9 +15,17 @@ class Site
         return (new View())->render('site.post', ['posts' => $posts]);
     }
 
-
     public function hello(): string
     {
         return new View('site.hello', ['message' => 'hello working']);
     }
+
+    public function signup(Request $request): string
+    {
+        if ($request->method==='POST' && User::create($request->all())){
+            return new View('site.signup', ['message'=>'Вы успешно зарегистрированы']);
+        }
+        return new View('site.signup');
+    }
+
 }
