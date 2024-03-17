3750a1e0bc743a3427c20071e71eed810dd4968d
DruDruDru <borisrasgard251@gmail.com>
2024-03-17 23:58:10 +0700

    добавление пакета collect в проект

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,4 @@ Route::add(['GET', 'POST'], '/login', [Controller\Site::class, 'login']);
 Route::add('GET', '/logout', [Controller\Site::class, 'logout']);
 Route::add(['GET', 'POST'], '/panel', [Controller\Site::class, 'panel'])
     ->middleware('auth');
+Route::add(['GET', 'POST'], '/', [Controller\Site::class, 'login']);
